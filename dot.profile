56f3b1ffd4452e894610f429491de1830e6b3502
rinrinne <rinrin.ne@gmail.com>
2013-01-17 15:07:47 +0900

    Clean up

--- dot.profile
+++ dot.profile
@@ -1,4 +1,9 @@
-if [ -d /usr/local/rbenv ]; then
-  PATH="/usr/local/rbenv:$PATH"
+#!/bin/sh
+
+RBENV_ROOT=/usr/local/rbenv
+
+if [ -d $RBENV_ROOT ]; then
+  export RBENV_ROOT
+  PATH="$RBENV_ROOT/bin:$PATH"
   eval "$(rbenv init -)"
 fi
--- install-shared-rbenv.sh
+++ install-shared-rbenv.sh
@@ -4,12 +4,11 @@ set -e
 
 [ `id -u` = 0 ] || (echo This script should be run as root; exit 1)
 [ -x /usr/bin/lsb_release ] || (echo This script should be run on Ubuntu; exit 1)
-[ `lsb_release -sr` = 12.10 ] || (echo This script should be run on Ubuntu 12.10; exit 1)
 
 [ ! -z $RBENV_ROOT ] || RBENV_ROOT=/usr/local/rbenv
 
 # install packages
-apt-get install build-essential libncurses5-dev libgdbm-dev libyaml-dev tk-dev libreadline-dev zlib1g-dev libssl-dev libffi-dev libssl1.0.0 git-core curl
+apt-get install build-essential libncurses-dev libgdbm-dev libyaml-dev tk-dev libreadline-dev zlib1g-dev libssl-dev libffi-dev libssl1.0.0 git-core curl
 
 mkdir -p $RBENV_ROOT
 cd $RBENV_ROOT
@@ -19,8 +18,6 @@ mkdir -p plugins
 git clone https://github.com/sstephenson/ruby-build.git plugins/ruby-build
 
 export RBENV_ROOT
-#eval "$($RBENV_ROOT/bin/rbenv init -)"
-
 $RBENV_ROOT/bin/rbenv install 1.9.3-p327
 $RBENV_ROOT/bin/rbenv rehash
 $RBENV_ROOT/bin/rbenv global 1.9.3-p327
